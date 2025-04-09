7f0e1ccf458bf7482b79a416fa06aaf58111bd1c
tylerconnelly100 <tyler.connelly@tcu.edu>
2025-04-09 16:09:52 -0500

    Replaced dataset, updated data preprocessing code to work with new dataset

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 Variable Definitions:
 
-X1: Transaction date (continuous)
-X2: House age (continuous, years)
-X3: Distance to nearest MRT station (continuous, meters)
-X4: Number of convenience stores (integer)
-X5: latitude (continuous, degrees)
-X6: longitude (continuous, degrees)
-Y: House price of unit area (continuous, 10000 New Taiwan Dollar per Ping)  
-   (1 Ping = 3.3 meters squared)
+Store ID: (Index) ID of the particular store.
+
+Store_Area: Physical Area of the store in yard square.
+
+Items_Available: Number of different items available in the corresponding store.
+
+Daily_Customer_Count: Number of customers who visited to stores on an average over month.
+
+Store_Sales: Sales in (US $) that stores made.
--- Real_Estate_Valuation.R
+++ Real_Estate_Valuation.R
@@ -1,19 +1,21 @@
 # Data Preprocessing
 
 # Importing the dataset
-dataset = read.csv('Real_Estate_Data.csv')
+dataset = read.csv('Stores.csv')
+# Remove Store ID Column
+dataset = dataset[, 2:5]
 
 # Splitting the dataset into the Training set and Test set
 library(caTools)
 # set.seed(123)
-split = sample.split(dataset$Y, SplitRatio = 0.8)
+split = sample.split(dataset$Store_Sales, SplitRatio = 0.8)
 training_set = subset(dataset, split == TRUE)
 test_set = subset(dataset, split == FALSE)
 
 # Feature Scaling
-training_scaled_cols = scale(training_set[, 1:6])
-training_set[, 1:6] = training_scaled_cols
-test_set[, 1:6] = scale(test_set[, 1:6], center=attr(training_scaled_cols, 'scaled:center'),
+training_scaled_cols = scale(training_set[, 1:4])
+training_set[, 1:4] = training_scaled_cols
+test_set[, 1:4] = scale(test_set[, 1:4], center=attr(training_scaled_cols, 'scaled:center'),
                         scale=attr(training_scaled_cols, 'scaled:scale'))
 
 # ------------------------------------------------------------------------------
