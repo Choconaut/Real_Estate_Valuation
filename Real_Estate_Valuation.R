f0196913c248cf498c8d55300610698f77080024
tylerconnelly100 <tyler.connelly@tcu.edu>
2025-04-13 15:48:17 -0500

    Added MLR

--- Real_Estate_Valuation.R
+++ Real_Estate_Valuation.R
@@ -7,7 +7,6 @@ dataset = dataset[, 2:5]
 
 # Splitting the dataset into the Training set and Test set
 library(caTools)
-# set.seed(123)
 split = sample.split(dataset$Store_Sales, SplitRatio = 0.8)
 training_set = subset(dataset, split == TRUE)
 test_set = subset(dataset, split == FALSE)
@@ -17,6 +16,49 @@ training_scaled_cols = scale(training_set[, 1:4])
 training_set[, 1:4] = training_scaled_cols
 test_set[, 1:4] = scale(test_set[, 1:4], center=attr(training_scaled_cols, 'scaled:center'),
                         scale=attr(training_scaled_cols, 'scaled:scale'))
+
+# ------------------------------------------------------------------------------
+# Multiple Linear Regression
+# ------------------------------------------------------------------------------
+
+# Splitting the dataset into the Training set and Test set
+library(caTools)
+split = sample.split(dataset$Store_Sales, SplitRatio = 0.8)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+#Building the optimal model using Backward Elimination
+mlr_regressor_opt = lm(formula = Store_Sales ~ .,
+                       data = training_set)
+summary(mlr_regressor_opt)
+mlr_regressor_opt = lm(formula = Store_Sales ~ Store_Area + Items_Available,
+                       data = training_set)
+summary(mlr_regressor_opt)
+mlr_regressor_opt = lm(formula = Store_Sales ~ Items_Available,
+                       data = training_set)
+summary(mlr_regressor_opt)
+# Optimal Team of Variables: Items_Available
+
+# Evaluating Model Performance
+mlr_sum = 0
+num_of_ind_vars = 1
+
+for (x in 1:10) {
+  split = sample.split(dataset$Store_Sales, SplitRatio = 0.8)
+  training_set = subset(dataset, split == TRUE)
+  test_set = subset(dataset, split == FALSE)
+  
+  mlr_regressor = lm(formula = Store_Sales ~ Items_Available,
+                     data = training_set)
+  mlr_y_pred = predict(mlr_regressor, newdata = test_set)
+  mlr_ssr = sum((test_set$Store_Sales - mlr_y_pred) ^ 2)
+  mlr_sst = sum((test_set$Store_Sales - mean(test_set$Store_Sales)) ^ 2)
+  mlr_r2 = 1 - (mlr_ssr/mlr_sst)
+  mlr_r2_adjusted = 1 - (1 - mlr_r2) * (length(test_set$Store_Sales) - 1) / (length(test_set$Store_Sales) - num_of_ind_vars - 1)
+  mlr_sum = mlr_sum + mlr_r2
+}
+mlr_avg = mlr_sum/10
+print(paste("Average R2: ", mlr_avg))
 
 # ------------------------------------------------------------------------------
 # Random Forest Regression
